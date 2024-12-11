d3d740ec80f66a32b0c59bf5e6c3127abde9f008
Ammarmyp <ammarmyp@gmail.com>
2024-12-11 10:39:34 +0300

    update import structure for user addresses

---
+++ src/controllers/user/index.ts
@@ -0,0 +1,7 @@
+import _createAddress from "./createAddress";
+
+namespace UserAddress {
+  export const createAddress = _createAddress;
+}
+
+export default UserAddress;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import createAddress from "../controllers/user/createAddress";
 import authMiddleware from "../middlewares/auth";
 import { errorHandler } from "../services/error-handler";
+import UserAddress from "../controllers/user";
 
 const addressRouter: Router = Router();
 
-addressRouter.post("/new", [authMiddleware], errorHandler(createAddress));
+addressRouter.post(
+  "/new",
+  [authMiddleware],
+  errorHandler(UserAddress.createAddress)
+);
 
 export default addressRouter;
