fe9f0a1b64e2067b9aebaede9f2b6ec668725850
Ammarmyp <ammarmyp@gmail.com>
2024-12-12 11:30:04 +0300

    fetch list of addresses for that specific user

---
+++ src/controllers/user/listAddresses.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import prisma from "../../../prisma/client";
+import ApiResponse from "../../services/apiResponse";
+
+const listAddresses = async (req: Request, res: Response) => {
+  const addresses = await prisma.address.findMany({
+    where: { id: req.user?.id },
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(true, "List of addresses fetched successfully", addresses)
+    );
+};
+
+export default listAddresses;
